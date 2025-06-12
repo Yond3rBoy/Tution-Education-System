d5dc9f9fd0e2751e96cc7cbe7e673661abe90070
Hendsemboi <dljf01@gmail.com>
2025-06-12 15:29:38 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,12 @@
+
+abstract class User {
+    protected String username;
+    protected String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public abstract void displayMenu();
+}
